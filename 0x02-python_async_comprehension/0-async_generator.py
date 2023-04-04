d50a2cf2da59532dfeca88757d1ff896dfcaa30f
Ikenna <ikenna@kali.Ikenna-PC>
2023-04-04 21:47:09 +0100

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""coroutine"""
+
+import asyncio
+import random
+from typing import Generator, AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    a coroutine function that takes no arguments
+    """
+
+    for item in range(10):
+        await asyncio.sleep(1)
+        yield(random.uniform(0, 10))
