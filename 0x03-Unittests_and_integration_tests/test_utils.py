d879e7e2b25568600045442c950a051fadee9146
Ikenna <ikenna@kali.Ikenna-PC>
2023-04-23 22:40:20 +0100

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,8 +4,8 @@ a TestAccessNestedMap class that inherits from unittest.TestCase
 """
 
 import unittest
-# from parameterized import parameterized, parameterized_class
-from utils import access_nested_map as nested_map
+from parameterized import parameterized
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -13,11 +13,13 @@ class TestAccessNestedMap(unittest.TestCase):
     Implement the TestAccessNestedMap.test_access_nested_map
     method to test that the method returns what it is supposed to
     """
-    # @parameterized.expand([])
-    def test_access_nested_map(self):
-        self.assertEqual(nested_map={"a": 1}, path=("a",))
-        self.assertEqual(nested_map={"a": {"b": 2}}, path=("a",))
-        self.assertEqual(nested_map={"a": {"b": 2}}, path=("a", "b"))
+    @parameterized.expand([
+        [{"a": 1}, ("a",), 1],
+        [{"a": {"b": 2}}, ("a",), {"b": 2}],
+        [{"a": {"b": 2}}, ("a", "b"), 2],
+    ])
+    def test_access_nested_map(self,  nested_map, path, expected_output):
+        self.assertEqual(access_nested_map(nested_map, path), expected_output)
 
 
 if __name__ == '__main__':
